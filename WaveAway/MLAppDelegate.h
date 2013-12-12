93d92cc67925c35b57b4dad0d039c1149b751323
Matt Long <matt.long@matthew-long.com>
2013-12-12 11:53:28 -0700

    Initial Commit

---
+++ WaveAway/MLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLAppDelegate.h
+//  WaveAway
+//
+//  Created by Matt Long on 12/12/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WaveAway/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  WaveAway
+//
+//  Created by Matt Long on 12/12/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
